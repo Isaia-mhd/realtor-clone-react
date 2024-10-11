6e024bd1ea222682be5269a7226c2cc6c1646532
Isaia-mhd <mohamedesaie21@gmail.com>
2024-10-11 14:30:19 +0200

    complete the Oauth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,46 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import { db } from '../firebase'
+import {toast} from 'react-toastify'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { useNavigate } from 'react-router'
 
 export default function OAuth() {
+
+  const navigate = useNavigate()
+  async function onGoogleClick(){
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      // check the user
+      const docRef = doc(db, "users", user.uid)
+      const docSnap = await getDoc(docRef)
+
+      if (!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+        navigate("/")
+      } else{
+        toast.error("User already registered !")
+        navigate("/sign-up")
+
+      }
+     
+
+    } catch (error) {
+      console.log(error);
+      toast.error("Couldn't authorize with Google");
+    }
+   
+  }
   return (
-    <button className='w-full bg-red-700 rounded px-7 py-3 text-white font-thin text-sm flex gap-2 items-center justify-center text-center uppercase hover:bg-red-800 active:bg-red-900 transition duration-150 ease-in-out'>
+    <button type='button' onClick={onGoogleClick} className='w-full bg-red-700 rounded px-7 py-3 text-white font-thin text-sm flex gap-2 items-center justify-center text-center uppercase hover:bg-red-800 active:bg-red-900 transition duration-150 ease-in-out'>
             <FcGoogle className='bg-white text-2xl rounded-full '/>
             Continue with Google
     </button>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,8 +11,8 @@ import { toast } from 'react-toastify';
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
+    name: "", 
+    email: "", 
     password: "",
   });
 
@@ -20,10 +20,10 @@ export default function SignUp() {
   const navigate = useNavigate()
   function onChange(e){
     setFormData((prevState)=>({ 
-      ...prevState,
+      ...prevState, 
       [e.target.id]: e.target.value, 
-      
     }));
+    
   }
 
   // mila asina async ny fonction rah ampiasa await
@@ -58,17 +58,17 @@ export default function SignUp() {
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
           <form onSubmit={onSubmit}>
-          <input className='my-3 w-full px-4 py-2 text-xl text-gray-500 bg-white border-gray-300 rounded transition ease-in-out' type="text" name="" id="name" value={name} onChange={onChange} placeholder='Full name'/>
+            <input className='my-3 w-full px-4 py-2 text-xl text-gray-500 bg-white border-gray-300 rounded transition ease-in-out' type="text" name="" id="name" value={name} onChange={onChange} placeholder='Full name'/>
 
             <input className='my-3 w-full px-4 py-2 text-xl text-gray-500 bg-white border-gray-300 rounded transition ease-in-out' type="email" name="" id="email" value={email} onChange={onChange} placeholder='Email adress'/>
 
             <div className='my-3 relative'>
-              <input className=' w-full px-4 py-2 text-xl text-gray-500 bg-white border-gray-300 rounded transition ease-in-out' type={showPassword ? "text" : "password"} name="" id="password" value={password} onChange={onChange} placeholder='Password'/>
+              <input className=' w-full px-4 py-2 text-xl text-gray-500 bg-white border-gray-300 rounded transition ease-in-out' type={showPassword ? "text" : "password" } name="" id="password" value={password} onChange={onChange} placeholder='Password'/>
 
               {showPassword ? (
                 <AiFillEyeInvisible className='absolute right-3 top-3 cursor-pointer' onClick={() => setShowPassword((prevState) => !prevState)}/>
               ) : (
-                <AiFillEye className='absolute right-3 top-3 cursor-pointer' onClick={() => setShowPassword((prevState) => !prevState)}/>
+                <AiFillEye className='absolute  right-3 top-3 cursor-pointer' onClick={() => setShowPassword((prevState) => !prevState)}/>
               ) }
             </div>
             <div className='my-3 w-full flex justify-between whitespace-nowrap text-sm sm:text-lg'>
