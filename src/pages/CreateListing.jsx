76fd4f2c4f387716375e7062c1377ba95dc9a0d6
Isaia-mhd <mohamedesaie21@gmail.com>
2024-12-02 15:21:23 +0100

    Create Share copy link

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -101,7 +101,7 @@ export default function CreateListing() {
 
             location = data.status === "ZERO_RESULTS" && undefined;
 
-            if(location == undefined){
+            if(location === undefined){
                 setLoading(false)
                 toast.error("Please enter a correct address !")
                 return; 
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,11 +6,15 @@ import  Spinner  from '../components/Spinner';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination} from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
+
+
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false);
     SwiperCore.use([Autoplay, Pagination, Navigation]);
     useEffect(()=>{
         async function fetchListing(){
@@ -41,6 +45,16 @@ export default function Listing() {
             </SwiperSlide>
         ))}
     </Swiper>
+    <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 w-12 h-12 rounded-full flex items-center justify-center" onClick={()=>{
+        navigator.clipboard.writeText(window.location.href);
+        setShareLinkCopied(true)
+        setTimeout(()=>{
+            setShareLinkCopied(false);
+        }, 2000)
+    }}>
+        <FaShare className="text-lg text-slate-5020" />
+    </div>
+    {shareLinkCopied && <p className="fixed top-[23%] right-[3%] border-2 border-gray-400 rounded-md bg-white z-10">Link Copied</p>}
   </main>
   
 }
